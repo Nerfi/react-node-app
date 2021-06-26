7b739eea5e5b40e39c0ba3e45bdadc20f6421081
Juan Paredes <juanchuu.jfpc@gmail.com>
2021-06-26 20:43:07 +0200

    got the country type by the user

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [country,setCountry] = useState('');
+
+  useEffect(() => {
+
+      const fetchBackEnd = async () => {
+
+        fetch("/api")
+         .then((res) => res.json())
+         .then((data) => setData(data.message))
+          .catch(e =>  console.log(e))
+      }
+
+      //caliing the function
+
+      fetchBackEnd();
+
+  },[]);
+
+  //handling change on input value
+
+  const handleChange = (e) => {
+
+    const value = e.target.value;
+    setCountry(value)
+
+  }
+
+
+  //submitting the data to backend
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //url to post data to
+    const url = `https://restcountries.eu/rest/v2/name/${country}`
+    fetch("api/" + country)
+      .then(res =>  res.json())
+      .then(res =>  console.log(res))
+
+
+    console.log(url);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={country} onChange={ handleChange}/>
+      <button type="submit">Search country</button>
+
+    </form>
+
     </div>
   );
 }
