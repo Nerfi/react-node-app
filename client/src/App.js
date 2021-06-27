e4dcc33d6646620395f8d5c6463cc7a309f8f7fa
Juan Paredes <juanchuu.jfpc@gmail.com>
2021-06-27 15:00:40 +0200

    retieving countries

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,22 @@ import {useState, useEffect} from 'react';
 
 function App() {
 
+  //this state must be delete afterwars
   const [data, setData] = useState(null);
+
   const [country,setCountry] = useState('');
+  const [error, setError] = useState(null);
+  const [renderCountry, setRenderCountry] = useState({});
 
-  useEffect(() => {
+  //handling multiples countryes
+  const [countriesResponse, setCountriesResponse] = useState([]);
+  //user input countries
+  const [countries, setCountries] = useState('');
 
-      const fetchBackEnd = async () => {
 
-        fetch("/api")
-         .then((res) => res.json())
-         .then((data) => setData(data.message))
-          .catch(e =>  console.log(e))
-      }
 
-      //caliing the function
-
-      fetchBackEnd();
-
-  },[]);
 
   //handling change on input value
-
   const handleChange = (e) => {
 
     const value = e.target.value;
@@ -32,32 +27,71 @@ function App() {
   }
 
 
-  //submitting the data to backend
+  //handling submmitn action
+const handleSubmit = (e) => {
+  //preventing default behavior
+  e.preventDefault();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //url to post data to
-    const url = `https://restcountries.eu/rest/v2/name/${country}`
-    fetch("api/" + country)
-      .then(res =>  res.json())
-      .then(res =>  console.log(res))
+  fetch( `/api/${country}`)
+    .then(res => res.json())
+    .then(res => setRenderCountry(res))
+    .catch(err => setError(err))
+
+    //cleaning the state after submitting
+    setCountry('');
 
 
-    console.log(url);
-  }
+}
 
+
+///second call for several countries
+const handleSubmitCountries = (e) => {
+        e.preventDefault();
+
+        const countriesToSend = countries.split('').map(s => s.slice(0,2))
+
+        //make a post request
+
+        fetch(`/api/${countries}`, {
+          method: 'POST',
+          headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+          },
+          body:JSON.stringify(countriesToSend)
+        }).then(res => res.json())
+          .then(res =>  console.log(res))
+          .catch(e => console.log(e))
+
+}
 
 
   return (
     <div className="App">
+    {error && error}
     <form onSubmit={handleSubmit}>
-      <input type="text" value={country} onChange={ handleChange}/>
+      <input type="text" value={country} onChange={handleChange} placeholder="search one or several countryies"/>
       <button type="submit">Search country</button>
 
     </form>
+
+    <form onSubmit={handleSubmitCountries}>
+    <input type="text" placeholder="countries" value={countries} onChange={(e) => setCountries(e.target.value)}/>
+    <button type="submit">Search for countries</button>
+    </form>
+
+
+    <div className="renderCountry">
+
+      <li>{renderCountry[0]?.name}</li>
+      <li>{renderCountry[0]?.capital}</li>
+      <li>{renderCountry[0]?.languages[0].nativeName}</li>
+
+    </div>
 
     </div>
   );
 }
 
 export default App;
+
