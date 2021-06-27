3cfc81915d64572ddae134ce2eba02dacb9e675f
Juan Paredes <juanchuu.jfpc@gmail.com>
2021-06-27 10:02:45 +0200

    install node fetch package

--- server/index.js
+++ server/index.js
@@ -5,10 +5,16 @@ const PORT = process.env.PORT || 3004;
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from ddserver!" });
 });
+
+app.get("/api/:country?", (req, res) => {
+  //destructuring reque object
+    const {country } = req.params;
+    console.log(country, 'request from front end ')
+})
 
 
 app.listen(PORT, () => {
-  console.log('nodemon?' + PORT)
+  console.log('adssadasfasdfasdSADFASDFasdnodemo' + PORT)
 })
