86ca3131d2e2300306b499bab727adb3ee7048cc
Juan Paredes <juanchuu.jfpc@gmail.com>
2021-07-08 19:24:59 +0200

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import LandingComponent from './components/Landing';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Navbar from './components/Navbar';
 
 
 
@@ -11,6 +12,7 @@ function App() {
   return(
     <>
     <div className="App">
+    <Navbar/>
     <Router>
 
       <Switch>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <>
+    <nav style={{display: 'flex', justifyContent: 'center'}}>
+      navbar working
+    </nav>
+    </>
+  )
+};
+
+export default Navbar;
